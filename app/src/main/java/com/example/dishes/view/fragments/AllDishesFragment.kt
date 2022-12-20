17cd9811e12febfd29eb2b520240217701198269
DAN BIZWA <bizwa_dan@BIZWAs-MacBook-Pro.local>
2022-12-21 00:06:14 +0100

    second commit

---
+++ app/src/main/java/com/example/dishes/view/fragments/AllDishesFragment.kt
@@ -0,0 +1,61 @@
+package com.example.dishes.view.fragments
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.*
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.dishes.R
+import com.example.dishes.databinding.FragmentAllDishesBinding
+import com.example.dishes.view.activities.AddUpdateDishActivity
+import com.example.dishes.viewmodel.HomeViewModel
+
+class AllDishesFragment : Fragment() {
+
+    private var _binding: FragmentAllDishesBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        val root = inflater.inflate(R.layout.fragment_all_dishes, container, false)
+        //: View = binding.root
+
+        val textView: TextView = root.findViewById(R.id.text_home)
+        homeViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_all_dishes,menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.action_add_dish -> {
+                startActivity(Intent(requireActivity(),AddUpdateDishActivity::class.java))
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+}
