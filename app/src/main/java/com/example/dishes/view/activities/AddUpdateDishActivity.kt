67a4277d4391f9eabdd3886f407ef62491f93d6b
DAN BIZWA <bizwa_dan@BIZWAs-MacBook-Pro.local>
2022-12-21 20:35:05 +0100

    third commit

--- app/src/main/java/com/example/dishes/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/example/dishes/view/activities/AddUpdateDishActivity.kt
@@ -1,12 +1,65 @@
 package com.example.dishes.view.activities
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.example.dishes.R
+import com.example.dishes.databinding.ActivityAddUpdateDishBinding
+import com.example.dishes.databinding.DialogCustomImageSelectionBinding
+import com.example.dishes.databinding.FragmentAllDishesBinding
 
-class AddUpdateDishActivity : AppCompatActivity() {
+class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var mBinding: ActivityAddUpdateDishBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_update_dish)
+        mBinding=ActivityAddUpdateDishBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        setupActionBar()
+        mBinding.ivAddDishImage.setOnClickListener(this)
+
+    }
+    private  fun setupActionBar(){
+        setSupportActionBar(mBinding.toolbarAddDishActivity)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        mBinding.toolbarAddDishActivity.setNavigationOnClickListener{
+            onBackPressed()
+        }
+    }
+
+    override fun onClick(v: View?) {
+
+
+       if (v != null){
+            when(v.id){
+                R.id.iv_add_dish_image ->{
+                    customimageSelectionDialog()
+                    //Toast.makeText(this,"You have clicked the ImageView",Toast.LENGTH_SHORT).show()
+                    return
+                }
+            }
+        }
+    }
+
+    private fun customimageSelectionDialog(){
+        val dialog=Dialog(this)
+        val binding:DialogCustomImageSelectionBinding= DialogCustomImageSelectionBinding.inflate(layoutInflater)
+        dialog.setContentView(binding.root)
+
+        binding.tvCamera.setOnClickListener{
+            Toast.makeText(this,"You have clicked the camera",Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+        binding.tvGallery.setOnClickListener{
+            Toast.makeText(this,"You have clicked the gallery",Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+
+        dialog.show()
+
     }
 }
